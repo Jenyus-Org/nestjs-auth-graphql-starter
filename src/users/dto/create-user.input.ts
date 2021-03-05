4bb3e968791b2850ce864f93af6afe00220dfd0c
Dan6erbond <moravrav@gmail.com>
2021-03-05 11:10:44 +0100

    Remove create user DTOs

--- src/users/dto/create-user.input.ts
+++
@@ -1,9 +0,0 @@
-import { InputType, OmitType } from "@nestjs/graphql";
-import { UserObject } from "./user.object";
-
-@InputType()
-export class CreateUserInput extends OmitType(
-  UserObject,
-  ["id"] as const,
-  InputType,
-) {}
--- src/users/dto/update-user.input.ts
+++ src/users/dto/update-user.input.ts
@@ -1,14 +1,14 @@
 import {
   InputType,
   IntersectionType,
+  OmitType,
   PartialType,
   PickType,
 } from "@nestjs/graphql";
-import { CreateUserInput } from "./create-user.input";
 import { UserObject } from "./user.object";
 
 @InputType()
 export class UpdateUserInput extends IntersectionType(
   PickType(UserObject, ["id"] as const, InputType),
-  PartialType(CreateUserInput),
+  PartialType(OmitType(UserObject, ["id"] as const, InputType)),
 ) {}
